d80dbcf0a4081c0f71854ea2ceb0a596a698eb8d
Nailykk <spatchoune@hotmail.fr>
2017-10-19 01:01:55 +0200

    Premières fonctions de stat

--- code/statistiques/Statistiques.java
+++ code/statistiques/Statistiques.java
@@ -2,8 +2,46 @@ import java.awt.* ;
 
 public class Statistiques
 {
-	DB interne = new DB('bohl', 'bohl', 'bohl')	;
+	InternalDB interne
 
-	float occupation() ;
-	float nonPresentation() ;
+
+	Statistiques()
+	{
+		 interne = new InternalDB()	;
+	}
+
+	float getOccupation1Day(date day)
+	{
+		float occupation ;
+
+		occupation = interne.getOccupationRate(day) ;
+
+		return occupation ;
+	}
+
+	float getOccupationRangeDay(date init, date last)
+	{
+		float occupation = 0 ;
+		date i ;
+
+		for(i=init ; i<=last ; i++)
+		{
+			if (occupation==0)
+				occupation+=getOccupation1Day(i) ;
+			else
+				occupation+=getOccupation1Day(i)/2 ;
+		}
+
+		return occupation ;
+
+
+	}
+
+
+	
+
+
+
+	interne.closeConnection() ;
+
 }
