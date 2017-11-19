2a3adffd1c552188165b305eb4dc47f5754dd998
Nailykk <spatchoune@hotmail.fr>
2017-11-19 20:13:16 +0100

    Modification de la table Historique + de l'évènement

--- code/Database/tables.sql
+++ code/Database/tables.sql
@@ -9,6 +9,7 @@ create table `Chambre`
 	primary key (Numero)
 ) ;
 
+#Première version Historique
 create table `Historique`
 (
 	jour date not null,
@@ -16,6 +17,17 @@ create table `Historique`
 	taux_non_presentation float,
 
 	primary key (jour)
+) ;
+
+#Deuxième table créée avec un identificateur
+create table `Historique`
+(
+	id int not null auto_increment,
+	jour date not null,
+	taux_occupation float,
+	taux_non_presentation float,
+
+	primary key (id)
 ) ;
 
 create table `Presentations`
@@ -28,6 +40,7 @@ create table `Presentations`
 ) ;
 
 
+#Première version de l'évènement
 create event save_history
 ON SCHEDULE EVERY 1 DAY STARTS '2017-10-20 00:01:00'
 do
@@ -37,3 +50,14 @@ do
 			(select COUNT(*) from bohl.Chambre where Occupee=1),
 			(select count(*) from bohl.Presentations where jour=(select DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)))
 	)
+
+create event save_history
+ON SCHEDULE EVERY 1 DAY STARTS '2017-11-20 00:01:00'
+do
+	INSERT into bohl.Historique(jour, taux_occupation, taux_non_presentation) values
+	(
+			(select DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)),
+			(select COUNT(*) from bohl.Chambre where Occupee=1),
+			(select count(*) from bohl.Presentations where jour=(select DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)))
+	)
+
